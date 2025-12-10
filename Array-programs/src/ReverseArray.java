3981f9887eccab8b9c7625f1df14faee2dd32dcc
Abhishek Jachak <abhishekjachak22@gmail.com>
2025-12-11 00:42:23 +0530

    reverse array

--- Array-programs/src/ReverseArray.java
+++ Array-programs/src/ReverseArray.java
@@ -1,19 +1,30 @@
 public class ReverseArray {
 
-    static void rev(Object[] a){
+    static void rev(String[] a){
 
-//        System.out.println(a);
-        for(int i=a.length-1;i>=0;i--){
-            System.out.print(a[i]+" ");
+        int left = 0, right = a.length-1;
+        for(String g:a){
+            System.out.print(g+" ");
+        }
+        while(left<right){
+            String temp = a[left];
+            a[left] = a[right];
+            a[right] = temp;
+            left++;
+            right--;
+        }
+        for(String g:a){
+            System.out.print(g+" ");
         }
     }
 
     public static void main(String[] args){
+
         int[] arr1 = {2,4,8,6,1,9};
         String[] arr2 = {"Ram", "Shyam", "Abhishek"};
-        Object[] arr3 = {true, "ram", 89, false};
+        Object[] arr3 = {true, "Shantanu", 89, false};
 
-        System.out.println("Hello!!");
-        rev(arr3);
+        System.out.println("Hello "+ arr3[1]);
+        rev(arr2);
     }
 }
