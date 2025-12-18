0ff40a32a68a0f3738d6e0afd84a23117290a57d
Abhishek Jachak <abhishekjachak22@gmail.com>
2025-12-18 11:06:20 +0530

    learning array quicklyyyyyyy

---
+++ Array-programs/src/RotateArray.java
@@ -0,0 +1,22 @@
+//Rotate array left/right
+
+
+public class RotateArray {
+
+    void printArray(){
+        System.out.println("Hii");
+    }
+
+    void rotateArray(int[] arr){
+
+    }
+
+    public static void main(String[] args){
+
+        int[] arr1 = {39,5,1,45,3,10,24,12,20,9};
+
+        RotateArray r = new RotateArray();
+        r.printArray();
+
+    }
+}
---
+++ Exercises-Problem/src/com/learn/arraylist/PlayWithAL.java
@@ -0,0 +1,19 @@
+package com.learn.arraylist;
+import java.util.ArrayList;
+
+public class PlayWithAL {
+    public static void main(String[] args){
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        arr1.add(45);
+        arr1.add(23);
+        arr1.add(2,67);
+
+        System.out.println(arr1);
+        System.out.println(arr1.set(2,22222));
+
+        System.out.println(arr1.get(2));
+
+        System.out.println(arr1.get(2));
+    }
+}
+//add, addAll, remove, removeAll, clear, contains, size, get, set, indexOf, iterator
